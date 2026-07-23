003bd5ef82ffe38be52fb1966a4568670e6fec60
Arshin <arshinmrelju@users.noreply.github.com>
2026-07-23 08:14:07 +0530

    Add GraphicCity 4-bar SVG favicon and update layout head tags

--- helpers/layout.js
+++ helpers/layout.js
@@ -21,6 +21,7 @@ function docHead(title, description, opts = {}) {
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=JetBrains+Mono:wght@400;500&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link rel="icon" type="image/svg+xml" href="/public/favicon.svg">
 <link rel="stylesheet" href="/public/css/styles.css">
 <script src="https://unpkg.com/htmx.org@1.9.12" defer></script>
 <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.1/dist/cdn.min.js" defer></script>
